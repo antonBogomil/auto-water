f3912ea041b010f499d26b768fd968f766d85068
AntonB <anton.bogomil777@gmail.com>
2019-05-07 19:01:17 +0300

    client buld

---
+++ server/client/build/precache-manifest.f6de4ac1771ce6f3c794673caa1f0fcf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "2b29321e55e69d7f874b",
+    "url": "/static/css/main.3b330d5e.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "215c0977a3110a7b843c",
+    "url": "/static/js/2.657bff28.chunk.js"
+  },
+  {
+    "revision": "d4f1c93e3bb31d2a1421fb6cc0e05c3c",
+    "url": "/static/media/frame1.d4f1c93e.jpeg"
+  },
+  {
+    "revision": "2b29321e55e69d7f874b",
+    "url": "/static/js/main.6577cf1c.chunk.js"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "75ab47ef896a85e4860714dacd516410",
+    "url": "/static/media/open-iconic.75ab47ef.svg"
+  },
+  {
+    "revision": "c44bf494257b528f3e4aec3359434945",
+    "url": "/index.html"
+  }
+];
